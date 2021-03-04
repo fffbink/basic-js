dd6424ef2ca1babc4c89625cae0cfa1842e5d90a
Roman Patsiaichuk <roman.shady@yandex.ru>
2021-03-05 02:38:12 +0300

    feat: implement count-cats&&dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(a) {
+  //throw new CustomError('Not implemented');
+  let i =0;  
+  let result= a.reduce(function(previousValue, item, index, array) {
+      a[index].forEach(function(item, index, array) {
+             if(item == '^^'){
+                i++;
+             }
+    });
+  }, 0);
+return i;
+
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+   // убираем не строки
+  let arr = members.filter((item, i, arr)=> typeof(item) == "string");
+
+  // удаляем пробелы/ преобразовываем в верхни регистр.
+let tmp = arr.map(function (el) {
+  return el.trim().toUpperCase();
+});
+        //сортируем
+       tmp.sort();
+
+  let result =  tmp.map((item, i, arr) => {
+  return item[0]});
+  return result.join("").toUpperCase();
+            };
+//npm run test test/dream-team.test.js
