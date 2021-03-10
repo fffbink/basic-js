d547bbaebe6ffbb37f08a752e05ce583ed4a9da3
Roman Patsiaichuk <roman.shady@yandex.ru>
2021-03-10 04:12:22 +0300

    feat: implement array_tran

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+ 
+    if (!Array.isArray(arr)) {
+        throw new Error();
+      }
+    
+    let copy = arr.slice();
+    
+    for(let index=0; index<copy.length; index++){   
+       
+    if(copy[index] === `--discard-next`){
+        
+            if((typeof(copy[index+1])=='undefined' || copy.length == 1)){
+                copy.splice(index, 1);
+                index--;
+            }
+    
+           else if((copy[index+2] == '--double-prev') || (copy[index+2] == '--discard-prev')){         
+              copy.splice(index, 3)
+                index--;
+            }
+            
+            else if((typeof(copy[index+1]))!='undefined'){
+            copy.splice(index, 2);
+            index--;
+            } 
+            
+         }
+     
+    if(copy[index] === `--double-next`){
+        if(((typeof(copy[index+1]))=='undefined') || (copy.length == 1) ){
+                copy.splice(index, 1);
+                index--;
+            }
+            
+            else if(typeof(copy[index+1])!='undefined'){
+                copy.splice(index, 1, copy[index+1]);
+                index--;    
+            }
+            
+        }
+    
+    if(copy[index] === `--discard-prev`){
+            
+            if(index == 0){
+            copy.splice(index, 1);  
+            index--;
+            }
+            
+            if(index>0){
+            copy.splice(index-1, 2); 
+            index--;  
+            } 
+        }
+    
+    if(copy[index] === `--double-prev`){
+            
+            if(index == 0){
+            copy.splice(index, 1); 
+            index--;        
+             }
+    
+            if(index>0){
+            copy.splice(index, 1, copy[index-1]);
+            index--;   
+        }
+      
+    }
+}
+    
+    return copy;  
 };
+
